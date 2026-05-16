42105a7b358e18e8c81da93a2b170436d7cfdf49
Harsh Pathak <harphy45@gmail.com>
2026-05-16 21:13:29 +0530

    hompage

---
+++ frontend/src/landing_page/home/Award.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+function Award() {
+    return ( <></>);
+}
+
+export default Award;
---
+++ frontend/src/landing_page/home/Home.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+function Homw() {
+    return ( <></> );
+}
+
+export default Homw;
