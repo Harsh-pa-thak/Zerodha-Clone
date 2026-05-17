a86f43cfc6ff5488c469032e81de5922aeb73605
Harsh Pathak <harphy45@gmail.com>
2026-05-17 23:43:00 +0530

    dashboard final

--- dashboard/src/Home.jsx
+++ dashboard/src/Home.jsx
@@ -1,14 +1,12 @@
-import React from "react";
-
 import Dashboard from "./Dashboard";
 import TopBar from "./TopBar";
 
 const Home = () => {
   return (
-    <>
+    <div className="app-shell">
       <TopBar />
       <Dashboard />
-    </>
+    </div>
   );
 };
 
